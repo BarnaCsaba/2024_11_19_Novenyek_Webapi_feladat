c93c2c3c7a8acacf2c9cac64ec88dd5a91a817bd
Csib_Csab <barna.csaba@diak.szbi-pg.hu>
2024-11-19 09:36:19 +0100

    all in all

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def index():
-    return render_template('index.html')
-
-@app.route('/tabla', methods=["GET"])
-def tabla():
-    return render_template('tabla.html')
 
 plants = [
     {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
@@ -16,3 +9,14 @@ plants = [
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
 
+@app.route('/', methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@app.route('/tabla', methods=["GET"])
+def tabla():
+    return render_template('tabla.html', plants=plants)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
